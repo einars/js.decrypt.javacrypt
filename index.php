1acff55a20c13f07df8ef47bfd385bb579d0a28f
Einar Lielmanis <einars@gmail.com>
2013-07-18 02:50:14 +0300

    Refactor, web face

---
+++ index.php
@@ -0,0 +1,132 @@
+<?php
+
+$message = null;
+$result = null;
+
+if (isset($_REQUEST['packed_source'])) {
+    $source = $_REQUEST['packed_source'];
+
+    if ($source) {
+        require_once('js.decrypt.javacrypt.php');
+
+        $result = decrypt_javacrypt($source);
+
+        if ($result === false) {
+            $result = $source;
+            $message = "This doesn’t look like JavaCrypt’ed file.";
+            if ($source and false !== strpos('JavaCrypt', $source)) {
+                $message = "Failed to unpack this file; would you please send me it for the examination?";
+            }
+        }
+    }
+
+}
+
+
+?>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>JavaCrypt unpacker</title>
+<style type="text/css">
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  font-size: 62.5%;
+}
+body {
+  font-family: "Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif;
+  font-size: 14px; font-size: 1.4rem;
+  line-height: 150%;
+}
+textarea, input {
+  font-family: "Droid Mono", "Monaco", "Lucida Console", "Courier New", monospace;
+}
+h1, button {
+  font-family: "Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif;
+  font-size: 24px; font-size: 2.4rem;
+}
+
+body {
+    background-color: #f4f4f4;
+}
+
+button {
+    width: 100%;
+    line-height: 150%;
+    cursor: pointer;
+}
+
+.source-input {
+    display: block;
+    border: 1px solid #ccc;
+    width: 100%;
+    height: 50%;
+    margin-top: 14px;
+    padding: 7px;
+    background-color: #ffe;
+}
+
+.unpacker-plate {
+    margin: 20px 40px;
+    border-radius: 10px;
+    border: 1px solid #aaa;
+    background-color: white;
+    padding: 20px;
+    box-shadow: 0 4px 10px 0 #ccc;
+}
+
+.message {
+    padding-top: 14px;
+    color: #933;
+}
+
+
+.submit {
+    margin-top: 7px;
+}
+.unpacker-form {
+    margin-bottom: 14px;
+}
+.footer {
+    color: #888;
+}
+
+</style>
+</head>
+
+<body>
+
+<div class="unpacker-plate">
+<p>This is an automatic JavaCrypt unpacker. I don’t know what packer produces this: packed objects were found in the wild.</p>
+<p>Paste yor javacrypted source inside the textarea.</p>
+
+<?php
+if ($message) {
+    printf('<p class="message">%s</p>', $message);
+}
+?>
+
+<form class="unpacker-form" method="post" action="?">
+<textarea spellcheck="false" name="packed_source" class="source-input js-focus"><?php echo htmlspecialchars($result); ?></textarea>
+<button class="submit" type="submit">Unpack</button>
+</form>
+
+<p class="footer">Written by Einar Lielmanis, einar@jsbeautifier.org</p>
+</div>
+
+<script>
+    if (document.getElementsByClassName) {
+        var elts = document.getElementsByClassName('js-focus');
+        elts[0].focus();
+    }
+</script>
+</body>
+</html>
+
+
---
+++ js.decrypt.javacrypt.php
@@ -0,0 +1,142 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+# javacrypt decryptor
+# --------------------------------
+# written by Einar Lielmanis, 2013
+# MIT licence
+#
+# web: http://spicausis.lv/js.decrypt.javacrypt
+# commandline: php js.decrypt.javacrypt.php packed_file
+# your own use (why would you?): $decrypted = decrypt_javacrypt($encrypted)
+#
+# Yes, this might be cool to add to jsbeautifier.org.
+# You’re welcome to port this to javascript, should be easy.
+
+if ( ! isset($_SERVER['REQUEST_URI']) and isset($argv[1])) {
+    // run from command-line
+    echo decrypt_javacrypt(file_get_contents($argv[1]));
+}
+
+function decrypt_javacrypt($source)
+{
+    $decryptor_source = null;
+    $encrypted = null;
+    $cryptkey = null;
+
+    $quote='["\']';
+    $maybe_quote='["\']?';
+
+    if (preg_match('/JavaCrypt\(' . $quote . '([\da-zA-Z]+)' . $quote . ',\s*' . $maybe_quote . '([\da-zA-Z]+)' . $maybe_quote . '\)/', $source, $matches)) {
+        $encrypted = $matches[1];
+        $cryptkey = $matches[2];
+    }
+    if (preg_match('/(func' . 'tion JavaC.*?;return;})/', $source, $matches)) {
+        $decryptor_source = $matches[1];
+    }
+
+    if ( ! $decryptor_source or ! $encrypted or ! $cryptkey) return false;
+
+    if (strpos($decryptor_source, '\\x2f\\x2a\\x2a\\x2f') === false) {
+        $version = 'v1';
+    } else {
+        $version = 'v2';
+    }
+
+    $cryptkey = (int)($cryptkey & 0xffffffff);
+
+
+    $table = array();
+    for ($w = 0; $w < 256; $w++) {
+        $a = $w;
+        for($y = 8; $y > 0; $y--) {
+            $x = ($a & 1) ? 0xedb88320 : 0;
+            if ($version === 'v1') $a = ($a >> 1) ^ $x;
+            if ($version === 'v2') $a = rsh($a, 1) ^ $x;
+        }
+        if ($version === 'v1') $table[ $w ] = $a;
+        if ($version === 'v2') $table[ $w ] = sgn_normalize($a);
+    }
+
+    $caller_source = $decryptor_source;
+
+    for ($iteration = 0; $iteration < 6; $iteration++) {
+
+        $initial_cryptkey = $cryptkey;
+        $caller_source = preg_replace('/\s+/', '', $caller_source);
+        $caller_source = str_replace('"', '', $caller_source);
+        $caller_source = str_replace('anonymous', '', $caller_source);
+        $caller_source = str_replace('});r', '}));r', $caller_source);
+        $caller_source = str_replace('n;})', 'n;}', $caller_source);
+
+        $caller_source = str_replace('/**/', '', $caller_source);
+
+
+        if ($version == 'v2') {
+            $caller_source = str_replace('func' . 'tionJ', 'func' . 'tion J', $caller_source);
+            $caller_source = str_replace("\x0a", '', $caller_source);
+        }
+
+        for ($w = 0; $w < strlen($caller_source); $w += 2) {
+            $cryptkey = ($cryptkey >> 8) ^ $table[ ($cryptkey ^ ord($caller_source[$w])) & 255 ];
+        }
+
+        if ($version == 'v1') $cryptkey = $cryptkey ^ 0xffffffff;
+        if ($version == 'v2') $cryptkey = sgn_normalize($cryptkey ^ 0x7ffffffe);
+
+        $caller_source = sprintf('fun' . 'ction(%s){%s}'
+            , $iteration == 0 ? 'obj' : sprintf('f%x', $initial_cryptkey)
+            , substr(
+                $decryptor_source,
+                $start = strpos($decryptor_source, 'arguments'),
+                strpos($decryptor_source, 'return') + 7 - $start
+            ));
+    }
+
+    $cryptkey = sprintf('%08X', $cryptkey);
+
+    $sog = array();
+    for($i = 0; $i < 8; $i++) {
+        $sog[$i] = ord($cryptkey[$i]);
+    }
+    $output = '';
+    $output_h = '';
+
+    $looks_good = true;
+    for($i = 0, $a = 0; $i < strlen($encrypted); $i += 2) {
+        $snip = substr($encrypted, $i, 2);
+        $aw = hexdec($snip) - $sog[$a];
+        if ($aw < 0) $aw += 256;
+        if ($aw > 256) $aw -= 256;
+        $output .= chr($aw);
+        if ($aw < 32 and $aw != 10 and $aw != 13 and $aw != 9) {
+            $looks_good = false;
+        }
+        $a = ($a + 1) % 8;
+    }
+
+    if ( ! $looks_good) {
+        return false;
+    } else {
+        return $output;
+    }
+
+}
+
+
+function rsh($a, $n)
+{
+    // alt. 32-bit shift right, keeping sign
+    return ($a >> 1) | ($a & 0x80000000);
+}
+
+function sgn_normalize($n)
+{
+    if ($n & 0x80000000) {
+        return ($n & 0x7fffffff) - 1;
+    } else {
+        return $n;
+    }
+}
